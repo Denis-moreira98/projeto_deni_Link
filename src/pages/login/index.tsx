198a9feaf8fda2b7d2c903c6598abf9fd5ae0ba7
Denis Moreira <silvad281@gmail.com>
2023-07-05 19:09:33 -0300

    feat: pagina de redes sociais

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -32,8 +32,8 @@ export function Login() {
       <div className="flex w-full h-screen items-center flex-col justify-center">
          <Link to="/">
             <h1 className="mt-11 text-white mb-7 font-bold text-5xl">
-               Dev
-               <span className="bg-gradient-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent">
+               Deni
+               <span className="bg-gradient-to-r from-blue-500 to-purple-800 bg-clip-text text-transparent">
                   Link
                </span>
             </h1>
--- src/pages/networks/index.tsx
+++ src/pages/networks/index.tsx
@@ -1,7 +1,98 @@
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { FormEvent, useEffect, useState } from "react";
+
+import { db } from "../../services/firebaseConnection";
+import { setDoc, doc, getDoc } from "firebase/firestore";
+
 export function Networks() {
+   const [facebook, setFacebook] = useState("");
+   const [gitHub, setGitHub] = useState("");
+   const [linkedIn, setLinkedIn] = useState("");
+
+   useEffect(() => {
+      function loadLinks() {
+         const docRef = doc(db, "social", "link");
+         getDoc(docRef)
+            .then((snapshot) => {
+               if (snapshot.data() !== undefined) {
+                  setFacebook(snapshot.data()?.facebook);
+                  setGitHub(snapshot.data()?.gitHub);
+                  setLinkedIn(snapshot.data()?.linkedIn);
+               }
+            })
+            .catch((err) => {
+               console.log("Erro ao carregar os links " + err);
+            });
+      }
+      loadLinks();
+   }, []);
+
+   function handleRegister(e: FormEvent) {
+      e.preventDefault();
+
+      setDoc(doc(db, "social", "link"), {
+         facebook: facebook,
+         gitHub: gitHub,
+         linkedIn: linkedIn,
+      })
+         .then(() => {
+            console.log("CADASTRADOS COM SUCESSO");
+         })
+         .catch((err) => {
+            console.log("ERRO AO SALVAR " + err);
+         });
+   }
+
    return (
-      <div>
-         <h1>REDES SOCIAIS</h1>
+      <div className="flex items-center flex-col min-h-screen pb-7 px-2">
+         <Header />
+
+         <h1 className="text-white text-2xl font-medium mt-8 mb-4">
+            Minhas redes sociais
+         </h1>
+
+         <form
+            onSubmit={handleRegister}
+            className="flex flex-col max-w-xl w-full"
+         >
+            <label className="text-white font-medium mt-2 mb-2">
+               Link do Facebook
+            </label>
+            <Input
+               type="url"
+               placeholder="Digite a url do facebook..."
+               value={facebook}
+               onChange={(e) => setFacebook(e.target.value)}
+            />
+
+            <label className="text-white font-medium mt-2 mb-2">
+               Link do GitHub
+            </label>
+            <Input
+               type="url"
+               placeholder="Digite a url do GitHub..."
+               value={gitHub}
+               onChange={(e) => setGitHub(e.target.value)}
+            />
+
+            <label className="text-white font-medium mt-2 mb-2">
+               Link do LinkedIn
+            </label>
+            <Input
+               type="url"
+               placeholder="Digite a url do LinkedIn..."
+               value={linkedIn}
+               onChange={(e) => setLinkedIn(e.target.value)}
+            />
+
+            <button
+               className="text-white  bg-blue-600 h-9 rounded-md items-center justify-center flex mb-7 font-medium"
+               type="submit"
+            >
+               Salvar Links
+            </button>
+         </form>
       </div>
    );
 }
